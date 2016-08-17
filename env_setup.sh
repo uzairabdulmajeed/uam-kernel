ae415daf38a0b2010eb4f4382772aca1406d1e6e
uzairabdulmajeed <uzair.xda@gmail.com>
2016-08-17 10:57:09 +0500

    Updated env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -65,18 +65,8 @@ export KCONFIG_NOTIMESTAMP=true
 export ARCH=arm64;
 export SUB_ARCH=arm64;
 
-# G920P
-if [ "$TARGET" = "G920P" ] ; then
-	export KERNEL_CONFIG="custom-zerofltespr_defconfig";
-fi;
-
-# G925P
-if [ "$TARGET" = "G925P" ] ; then
-	export KERNEL_CONFIG="custom-zeroltespr_defconfig";
-fi;
-
-# G920T
-if [ "$TARGET" = "G920T" ] ; then
+# G920F
+if [ "$TARGET" = "G920F" ] ; then
 	export KERNEL_CONFIG="custom_defconfig";
 fi;
 
